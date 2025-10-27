9c7a20ae32171ed8238279304ba271aef260d50f
Andrés Mejía <amejia@uamv.edu.ni>
2025-10-27 10:24:39 -0600

    Empresa Mensajeria

---
+++ EmpresaMensajeria/EmpresaMensajeria/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace EmpresaMensajeria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbProducto = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.chUrgente = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbRegulares = new System.Windows.Forms.Label();
+            this.lbUrgentes = new System.Windows.Forms.Label();
+            this.btnDespachar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del producto:";
+            // 
+            // tbProducto
+            // 
+            this.tbProducto.Location = new System.Drawing.Point(193, 39);
+            this.tbProducto.Name = "tbProducto";
+            this.tbProducto.Size = new System.Drawing.Size(269, 26);
+            this.tbProducto.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(193, 137);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(157, 44);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // chUrgente
+            // 
+            this.chUrgente.AutoSize = true;
+            this.chUrgente.Location = new System.Drawing.Point(193, 88);
+            this.chUrgente.Name = "chUrgente";
+            this.chUrgente.Size = new System.Drawing.Size(93, 24);
+            this.chUrgente.TabIndex = 4;
+            this.chUrgente.Text = "Urgente";
+            this.chUrgente.UseVisualStyleBackColor = true;
+            this.chUrgente.CheckedChanged += new System.EventHandler(this.chUrgente_CheckedChanged);
+            this.chUrgente.Click += new System.EventHandler(this.chUrgente_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Productos Regulares:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Productos Urgentes:";
+            // 
+            // lbRegulares
+            // 
+            this.lbRegulares.AutoSize = true;
+            this.lbRegulares.Location = new System.Drawing.Point(26, 281);
+            this.lbRegulares.Name = "lbRegulares";
+            this.lbRegulares.Size = new System.Drawing.Size(108, 20);
+            this.lbRegulares.TabIndex = 7;
+            this.lbRegulares.Text = "Sin Productos";
+            // 
+            // lbUrgentes
+            // 
+            this.lbUrgentes.AutoSize = true;
+            this.lbUrgentes.Location = new System.Drawing.Point(255, 281);
+            this.lbUrgentes.Name = "lbUrgentes";
+            this.lbUrgentes.Size = new System.Drawing.Size(108, 20);
+            this.lbUrgentes.TabIndex = 8;
+            this.lbUrgentes.Text = "Sin Productos";
+            // 
+            // btnDespachar
+            // 
+            this.btnDespachar.Location = new System.Drawing.Point(198, 358);
+            this.btnDespachar.Name = "btnDespachar";
+            this.btnDespachar.Size = new System.Drawing.Size(152, 62);
+            this.btnDespachar.TabIndex = 9;
+            this.btnDespachar.Text = "Despachar";
+            this.btnDespachar.UseVisualStyleBackColor = true;
+            this.btnDespachar.Click += new System.EventHandler(this.btnDespachar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 450);
+            this.Controls.Add(this.btnDespachar);
+            this.Controls.Add(this.lbUrgentes);
+            this.Controls.Add(this.lbRegulares);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chUrgente);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbProducto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "App mensajeria";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbProducto;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.CheckBox chUrgente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbRegulares;
+        private System.Windows.Forms.Label lbUrgentes;
+        private System.Windows.Forms.Button btnDespachar;
+    }
+}
+
---
+++ EmpresaMensajeria/EmpresaMensajeria/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmpresaMensajeria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Pila (Stack) para los pedidos urgentes → LIFO (último en entrar, primero en salir)
+
+        Stack<string> Urgentes = new Stack<string>();
+        // Cola (Queue) para los pedidos regulares → FIFO (primero en entrar, primero en salir)
+
+        Queue<string> Regulares = new Queue<string>();
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+
+        {
+            // Obtiene el texto ingresado en el cuadro de texto
+
+            string producto = tbProducto.Text.ToLower();
+            // Verifica que el usuario haya ingresado un producto
+
+            if (string.IsNullOrEmpty(producto))
+            {
+                MessageBox.Show("Ingrese un producto antes de agregar.");
+                return;
+            }
+            // Si la casilla de urgente está marcada, agrega el producto a la pila de urgentes
+
+            if (chUrgente.Checked)
+            {
+                Urgentes.Push(producto);
+                MostrarUrgentes();
+            }
+            // Si no está marcada, se agrega a la cola de regulares
+
+            else
+            {
+                Regulares.Enqueue(producto);
+                
+                mostrarRegulares();
+            }
+
+            tbProducto.Clear();
+            tbProducto.Focus();
+        }
+
+        private void chUrgente_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chUrgente_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mostrarRegulares()
+        {
+            // Si hay elementos en la cola, los muestra uno debajo del otro
+
+            if (Regulares.Count > 0)
+                lbRegulares.Text = string.Join(Environment.NewLine, Regulares);
+            else
+                lbRegulares.Text = "(sin pedidos regulares)";
+        }
+
+        private void MostrarUrgentes()
+        {
+            // Si hay elementos en la pila, los muestra uno debajo del otro
+
+            if (Urgentes.Count > 0)
+                lbUrgentes.Text = string.Join(Environment.NewLine, Urgentes);
+            else
+                lbUrgentes.Text = "(sin pedidos urgentes)";
+        }
+
+        private void btnDespachar_Click(object sender, EventArgs e)
+        {
+            // Si hay urgentes, se atiende primero la pila
+            if (Urgentes.Count > 0)
+            {
+                string atendido = Urgentes.Pop();
+                MessageBox.Show($"Se despachó urgente: {atendido}");
+                MostrarUrgentes(); // actualiza el label
+            }
+            // Si no hay urgentes, atiende un regular
+            else if (Regulares.Count > 0)
+            {
+                string atendido = Regulares.Dequeue();
+                MessageBox.Show($"Se despachó regular: {atendido}");
+                mostrarRegulares(); // actualiza el label
+            }
+            else
+            {
+                MessageBox.Show("No hay productos por despachar.");
+            }
+
+        }
+        
+    }
+}
